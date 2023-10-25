ebb79c1bfa44d6b50bdcab3cf588c77d75cf2e09
KIPP Jax DataTeam <135723595+KippJax-DT@users.noreply.github.com>
2023-10-25 12:19:58 -0400

    updated versiion adding another file

--- lambda_gspread_function/app.py
+++ lambda_gspread_function/app.py
@@ -7,10 +7,10 @@ import os
 # import necessary packages
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+    
+
 
 def lambda_handler(event,context):
-    
-   
     """  
     First we would like to read the file that is in our s3 bucket.
     We use boto3.resource to be able to do this. Once that has occured, we are able to download the file to our docker container
@@ -30,24 +30,34 @@ def lambda_handler(event,context):
 
     # cloud_file_name = os.environ.get('file_path')
     cloud_file_name =  os.environ.get('file_path')
-
+    sm_file = os.environ.get('sm_file')
+    
 
     logger.info(f"file: {cloud_file_name}")
     logger.info(f"Reading Bucket: {bucket_name}")
+    logger.info(f"file: {sm_file}")
 
     assess_object = s3.get_object(Bucket = bucket_name, Key = cloud_file_name)
     assess_body = assess_object['Body'].read().decode('utf-8')
+    
+    standards_object = s3.get_object(Bucket= bucket_name, Key = sm_file)
+    standards_body = standards_object['Body'].read().decode('utf-8')
+    
 
     # Create Stringio reading object
 
     assess_io = StringIO(assess_body)
+    sm_io = StringIO(standards_body)
 
     assessments_df = pd.read_csv(assess_io)
+    sm_df = pd.read_csv(sm_io)
     
     logger.info(f"Data loaded from S3. First 5 rows:\n{assessments_df.head()}")
     
     logger.info(f"There are {assessments_df.shape[0]} different types of assessments and code scopes.")
     assessments_df.to_csv(f"/tmp/assessments.csv")
+    sm_df.to_csv("/tmp/standards.csv")
+    
     #  --------------------------
 
     #  Next we want to use gspread to be albe to deposit the csv as a google sheet that is in our drive. 
@@ -65,8 +75,8 @@ def lambda_handler(event,context):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(cred_file_name,scope)
     client = gspread.authorize(credentials)
     
-    Assessments_file = os.environ.get("google_file")
-
+    
+    Assessments_file = os.environ.get("google_file")    
     spreadsheet = client.open(Assessments_file)
 
     with open('/tmp/assessments.csv', 'r') as file_obj:
@@ -76,3 +86,15 @@ def lambda_handler(event,context):
     logger.info(f"output: {spreadsheet}")
     
     print(f"Google file {Assessments_file} has been updated.")
+    
+    standards_file = os.environ.get("assessment_titles")
+    
+    spreadsheet = client.open(standards_file)
+    with open('/tmp/standards.csv', 'r') as file_obj:
+        content = file_obj.read()
+        client.import_csv(spreadsheet.id, data=content)
+        
+    logger.info(f"Output : {spreadsheet}")
+
+    
+    
